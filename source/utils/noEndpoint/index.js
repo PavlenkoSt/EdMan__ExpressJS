5f49412b817f16130a074a9fe9ec7e5f12e264e6
Stanislav <verbatim8872@gmail.com>
2023-03-18 14:47:13 +0200

    update noEndpoint mesage

--- source/utils/noEndpoint/index.js
+++ source/utils/noEndpoint/index.js
@@ -1,7 +1,8 @@
 import { NotFoundError } from '../errors';
 
 export const noEndpoint = (req, res, next) => {
-  const error = new NotFoundError('Enpoint not found');
+  console.log('req', req);
+  const error = new NotFoundError(`Enpoint not found -> ${req.method} ${req.originalUrl}`);
 
   next(error);
 };
