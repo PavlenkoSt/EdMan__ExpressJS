c19904b4f6ff285c8e40c7e280cad9f9a5feac79
Stanislav <verbatim8872@gmail.com>
2023-03-18 14:28:53 +0200

    path

--- source/utils/index.js
+++ source/utils/index.js
@@ -2,5 +2,5 @@ export { getPort, getDBUrl, getPassword } from './env';
 export { sessionOptions } from './options';
 export { rateLimiter } from './rateLimiter';
 export { validateBody, validateQuery } from './validate';
-export { authMiddleware } from './authMiddleware';
+export { authMiddleware } from './auth';
 export { loggerMiddleware, errorLoggerMiddleware } from './logger';
